a665ea8014d8e5dbb55b783fa208b45a3195b991
Mahfuzul Haque <MHaque@mathematica-mpr.com>
2021-01-08 01:37:51 -0500

    created marjana's fundmynosejob website

---
+++ modal.js
@@ -0,0 +1,30 @@
+var modal, btn, span;
+
+window.onload = function() {
+    // Get the modal
+    modal = document.getElementById("modal");
+
+    // Get the button that opens the modal
+    btn = document.querySelector(".venmo-btn");
+
+    // Get the <span> element that closes the modal
+    span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "flex";
+        modal.style.alignItems = 'center';
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
