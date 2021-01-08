122fe78151dbaa38fbb961f77a9f59576f9c21e0
Mahfuzul Haque <MHaque@mathematica-mpr.com>
2021-01-08 02:57:57 -0500

    added deeplinking

--- modal.js
+++ modal.js
@@ -1,5 +1,15 @@
 var modal, btn, span;
 
+function getOS() {
+  const userAgent = navigator.userAgent;
+  if(/android/i.test(userAgent)){
+      return 'android';
+  }
+  if(/iPad|iPhone|iPod/i.test(userAgent)){
+      return 'ios';
+  }
+}
+
 window.onload = function() {
     // Get the modal
     modal = document.getElementById("modal");
@@ -19,6 +29,19 @@ window.onload = function() {
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+    }
+
+    switch (getOS()) {
+      case 'android':
+        document.querySelector('.venmo-link').setAttribute('href', 'intent://venmo.com/marjana-begum#Intent;package=com.venmo;scheme=https;end');
+        break;
+      case 'ios':
+        document.querySelector('.venmo-link').setAttribute('href', 'venmo://users/36709575');
+        break;
+      default:
+        document.querySelector('.venmo-link').setAttribute('href', 'https://venmo.com/marjana-begum');
+        document.querySelector('.venmo-link').setAttribute('target', '_blank');
+        break;
     }
 }
 
